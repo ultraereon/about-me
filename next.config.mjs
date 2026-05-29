92da1a64fe7139dd67a1c9c0ed59b0a035a9a90f
ultraereon <arifmohammedashraf@gmail.com>
2026-05-29 16:49:40 +0530

    fix: Add GitHub pages config to next.config.mjs

--- next.config.mjs
+++ next.config.mjs
@@ -1,5 +1,9 @@
+const isProd = process.env.NODE_ENV === 'production';
+
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  output: 'export',
+  basePath: isProd ? '/about-me' : '',
   typescript: {
     ignoreBuildErrors: true,
   },
